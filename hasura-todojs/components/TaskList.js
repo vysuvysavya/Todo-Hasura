235a35e776cfff5928ae6638fc9fff68afbe2fbf
vysavya <vysu.vysavya@gmail.com>
2025-01-27 20:26:51 +0530

    cleaned files

--- hasura-todojs/components/TaskList.js
+++ hasura-todojs/components/TaskList.js
@@ -241,7 +241,6 @@ if (loading) {
         </button>
       </div>
 
-      {/* Update Task Modal */}
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50">
           <div className="bg-white rounded-lg p-6 w-96">
