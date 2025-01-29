9cfd6adce60af59749896ae548f36351b8f0fd22
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2025-01-29 17:33:10 +0530

    Update page.js

--- hasura-todojs/app/page.js
+++ hasura-todojs/app/page.js
@@ -1,10 +1,8 @@
 "use client";
 import "./globals.css";
 import TaskList from "@/components/TaskList";
-import { useState } from "react";
 
 export default function Home() {
-  const [filter, setFilter] = useState("all");
   return (
     <main className="max-w-2xl mx-auto p-4 space-y-6">
       <TaskList />
